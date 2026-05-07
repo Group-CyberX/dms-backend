3cc4b77320d6d90a3a6da1cd80194164ee2cf358
B M Janith Bhashitha <bhashithabmj@gmail.com>
2026-05-07 19:30:20 +0530

    Remove CrossOrigin annotation from DocumentController
    Removed CrossOrigin annotation from DocumentController.

--- src/main/java/com/dms/rest/DocumentController.java
+++ src/main/java/com/dms/rest/DocumentController.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/documents")
 public class DocumentController {
 
